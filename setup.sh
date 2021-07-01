3fe2a18b6991eba3d301b60455bf60cf1f99f092
Jason Asselin <asselin.jason@gmail.com>
2021-06-30 23:06:32 -0400

    emacs

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/vim ~/.vim
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/task/taskrc ~/.taskrc
+ln -sf ~/dotfiles/emacs/emacs ~/.emacs
+ln -sf ~/dotfiles/emacs/emacs.d ~/.emacs.d
