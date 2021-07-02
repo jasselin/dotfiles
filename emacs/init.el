d643ca942cfb70f52764076c97d45c11ba39ef7f
Jason Asselin <asselin.jason@gmail.com>
2021-07-02 10:52:22 -0400

    .

--- emacs/init.el
+++ emacs/init.el
@@ -136,3 +136,16 @@
 
 ;; Default directory
 (cd "~/")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(projectile dashboard doom-modeline doom-themes all-the-icons which-key evil-collection evil general use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/vim ~/.vim
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/task/taskrc ~/.taskrc
-ln -sf ~/dotfiles/emacs/emacs ~/.emacs
+ln -sf ~/dotfiles/emacs ~/.emacs.d
